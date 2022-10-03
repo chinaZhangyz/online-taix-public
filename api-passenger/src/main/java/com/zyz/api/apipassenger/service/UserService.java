a79768522901d1187cf54964a0825843d4fec112
zhangyuzhang <chinazhangyz@126.com>
2022-10-03 19:10:24 +0800

    通过accesstoken 查询手机号

--- api-passenger/src/main/java/com/zyz/api/apipassenger/service/UserService.java
+++ api-passenger/src/main/java/com/zyz/api/apipassenger/service/UserService.java
@@ -2,6 +2,9 @@ package com.zyz.api.apipassenger.service;
 
 import com.zyz.internalcommon.dto.PassengerUser;
 import com.zyz.internalcommon.dto.ResponseResult;
+import com.zyz.internalcommon.dto.TokenResult;
+import com.zyz.internalcommon.response.TokenResponse;
+import com.zyz.internalcommon.util.JwtUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -12,9 +15,14 @@ public class UserService {
         //解析accessToken 拿到手机号
         log.info("accesstoken:"+accessToken);
         //根据手机号查询用户信息
+        TokenResult tokenResult = JwtUtils.checkToken(accessToken);
+        String phone = tokenResult.getPhone();
+        log.info("手机号："+phone);
+
         PassengerUser passengerUser = new PassengerUser();
         passengerUser.setPassengerName("张山");
         passengerUser.setProfilePhoto("头像");
+        passengerUser.setProfilePhoto(phone);
         return ResponseResult.success(passengerUser);
     }
 }
