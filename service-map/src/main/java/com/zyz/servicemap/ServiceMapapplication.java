e595735ef5775bf7c7fca2078b449c59e513c07a
zhangyuzhang <chinazhangyz@126.com>
2022-10-17 18:17:50 +0800

    service-map骨架搭建

---
+++ service-map/src/main/java/com/zyz/servicemap/ServiceMapapplication.java
@@ -0,0 +1,11 @@
+package com.zyz.servicemap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceMapapplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceMapapplication.class,args);
+    }
+}
